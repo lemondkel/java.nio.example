c3074856ab8a735b7e941d58141c304cc22ba157
Lee du-jong <Lee du-jong@DESKTOP-726U58V>
2018-07-03 09:30:05 +0900

    OpenOption, PathMatcher

---
+++ src/example/java/nio/file/OpenOptionExample.java
@@ -0,0 +1,11 @@
+package example.java.nio.file;
+
+import java.nio.file.OpenOption;
+
+public class OpenOptionExample {
+	OpenOption s; // PathMatcher is an Interface;
+
+	// Objects of this type are used by methods such as newOutputStream,
+	// newByteChannel, FileChannel.open, and AsynchronousFileChannel.open when
+	// opening or creating a file.
+}
---
+++ src/example/java/nio/file/PathMatcherExample.java
@@ -0,0 +1,12 @@
+package example.java.nio.file;
+
+import java.nio.file.PathMatcher;
+
+public class PathMatcherExample {
+
+	PathMatcher pathMatcher; // PathMatcher is an Interface;
+	// have 1 method.
+	// abstract boolean matches(java.nio.file.Path)
+	
+	// for example, using AntPathMatcher with Springframework.
+}
