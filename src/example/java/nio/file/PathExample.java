319590b14a2654e251700d47d9e360fdec2fc52f
Lee du-jong <Lee du-jong@DESKTOP-726U58V>
2018-07-03 09:19:25 +0900

    path example!

---
+++ src/example/java/nio/file/PathExample.java
@@ -0,0 +1,25 @@
+package example.java.nio.file;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+public class PathExample {
+	public static void main(String[] args) {
+		Path path = Paths.get("/src/java/nio/example/file/PathExample.java");
+		System.out.println("File Name: " + path.getFileName());
+		System.out.println("Parent Directory Name: " + path.getParent().getFileName());
+		System.out.println("Directory Count: " + path.getNameCount());
+
+		System.out.println();
+		for (int i = 0; i < path.getNameCount(); i++) {
+			System.out.println(path.getName(i));
+		}
+		System.out.println();
+		Iterator<Path> iterator = path.iterator();
+		while (iterator.hasNext()) {
+			Path tmp = iterator.next();
+			System.out.println(tmp.getFileName());
+		}
+	}
+}
